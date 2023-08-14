45cdee5f38e92783ff943ef578d125d4b7ba89ab
Gabriel-Vieira-Franca <141461466+Gabriel-Vieira-Franca@users.noreply.github.com>
2023-08-14 19:45:38 -0300

    Primeira versão do programa de partida

---
+++ placar de jogo.py
@@ -0,0 +1,11 @@
+timeA= input( "Nome do time A: ")
+golstimeA= int(input ("Quantos gols o time A fez: "))
+timeB= input( "Nome do tima B: ")
+golstimeB = int(input ("Qunatos gols o time B fez: "))
+
+if golstimeA > golstimeB:
+    print (f"{timeA} venceu com diferença de {golstimeA- golstimeB} gols.")
+elif golstimeA < golstimeB:
+    print (f" {timeB} venceu com diferença de {golstimeB-golstimeA} gols.")
+elif golstimeA == golstimeB:
+    print ("O jogo terminou empatado.")
